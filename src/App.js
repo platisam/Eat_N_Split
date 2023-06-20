b0d69e1d0bccb0fa60cf2fe76ac4449cd7fe8b60
platisam <platisam@gmail.com>
2023-06-20 22:02:03 +0200

    setting conditional rendering in Friend component

--- src/App.js
+++ src/App.js
@@ -45,6 +45,18 @@ function Friend({ friend }) {
     <li>
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
     </li>
   );
 }
