09c6fbd6d7a35f85ba33fede2207b4f6c9a7f5bc
platisam <platisam@gmail.com>
2023-06-21 12:12:16 +0200

    setting selectedFriend props in FormSplitBill component

--- src/App.js
+++ src/App.js
@@ -149,7 +149,7 @@ function FormAddFriend({ onAddFriend }) {
 function FormSplitBill({ selectedFriend }) {
   return (
     <form className="form-split-bill">
-      <h2>Split a bill with X</h2>
+      <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💰 Bill value</label>
       <input type="text" />
@@ -157,13 +157,13 @@ function FormSplitBill({ selectedFriend }) {
       <label>🕴️ Your expense</label>
       <input type="text" />
 
-      <label>👯 X's expense</label>
+      <label>👯 {selectedFriend.name}'s expense</label>
       <input type="text" disabled />
 
       <label>🤑 Who is paying the bill</label>
       <select>
         <option value="user">You</option>
-        <option value="friend">X</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
 
       <Button>Split bill</Button>
